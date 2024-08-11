a615826458f651e3c3bca1d0dcc12e76fd152a9d
HazelnutStudio <hazelnutstudio.yt@gmail.com>
2024-08-11 15:10:45 +0800

    counting stats!!

--- bot.py
+++ bot.py
@@ -6,10 +6,11 @@ import two
 import random
 import counting
 import log
-
+from discord import app_commands
+import json
 load_dotenv()
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = str(os.getenv('DISCORD_TOKEN'))
 
 
 intents = discord.Intents.default()
@@ -17,12 +18,17 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+tree = app_commands.CommandTree(client)
+dummy_user = client.fetch_user(0)
+
 @client.event
 async def on_ready():
     print(f'logged in as {client.user}!')
     await counting.initialize(client)
     await log.initialize(client)
-    await log.info("bot: Hazelbot is online!")    
+    await log.info("bot: Hazelbot is online!") 
+    await tree.sync(guild=discord.Object(id=1232662729047801928))
+    await log.info("bot: tree synced!")
 
 @client.event
 async def on_message(message):
@@ -55,6 +61,42 @@ async def bot_interactions(message):
         options = ["did someone say my name?", "hey ;]", "hello!!", ":3", "SHUT THE ###### #### ##### ###"]
         rand = random.randint(0, len(options) - 1)
         await message.channel.send(options[rand])
+
+@tree.command(name="cstats",description="Get statistics for counting minigame", guild=discord.Object(id=1232662729047801928))
+@app_commands.describe(user="Get stats for a specific user")
+async def cstats(interaction, user:discord.User = None):
+    save = await counting.get_savefile()
+    if user == None:
+        content = f"**Highest Count:** {save['st_highest_count']}"
+        if save['st_ruinedby'] == "":
+            content += "\n**Ruined by**: (count in progress)"
+        else:
+            content += f"\n**Ruined by:** <@{save['st_ruinedby']}>"
+        content += f"\n**Total Counts:** {save['st_counts']}"
+        content += f"\n**Total Failures:** {save['st_failures']}"
+        embed = discord.Embed(colour = discord.Colour.from_str("#87ffc9"), title = "Counting Stats - Overall", description = content)
+        await interaction.response.send_message(embed=embed, ephemeral=False)
+    else:
+        user_stats = save.get(f"st_user_{user.id}")
+        content = ""
+        if user_stats == None:
+            content = "User has not interacted with the counting system."
+        else:
+            user_stats = dict(user_stats)
+            content = f"**Highest Count:** {user_stats.get('highest_count', 0)}"
+            content += f"\n**Total Counts:** {user_stats.get('total_counts', 0)}"
+            content += f"\n**Biggest Failure:** {user_stats.get('biggest_failure', 0)}"
+            content += f"\n**Total Failures:** {user_stats.get('failures', 0)}"
+        embed = discord.Embed(colour = discord.Colour.from_str("#87ffc9"), title = f"Counting Stats - {user.name}", description = content)
+        embed.set_thumbnail(url=str(user.display_avatar))
+        await interaction.response.send_message(embed=embed)
+@tree.command(name="clogsave",description="Log the save file for the counting minigame (for debug purposes)", guild=discord.Object(id=1232662729047801928))
+@app_commands.default_permissions(administrator=True)
+async def clogsave(interaction):
+    save = await counting.get_savefile()
+    save_json = json.dumps(save)
+    await interaction.response.send_message(f"```json\n{save_json}\n```")
+
 
 client.run(TOKEN)
 
--- counting.py
+++ counting.py
@@ -2,6 +2,7 @@ import discord
 import re
 import log
 import json
+import os
 
 CHANNEL_NAME = "counting"
 SAVE_FILE = "counting-stats.txt"
@@ -21,8 +22,8 @@ async def initialize(_client):
         number = save_contents['number']
         last_author = save_contents['last_author']
     else:
-        save_contents = {'number':0, 'last_author':0}
-        number = 0
+        save_contents = {'number':1, 'last_author':0, 'st_highest_count':0, 'st_failures':0, 'st_counts':0}
+        number = 1 
         last_author = 0
 
     
@@ -34,12 +35,26 @@ async def on_message(message):
     if message.channel.name != CHANNEL_NAME:
         return
 
+    user_stats = save_contents.get(f"st_user_{message.author.id}")
+    if user_stats == None:
+        user_stats = {}
+
+
     message_valid = await is_message_valid(message)
     if message_valid == 0:
         await message.add_reaction("❌")
+        if save_contents['st_highest_count'] == number - 1:
+            save_contents.update({'st_ruinedby':message.author.id})
+            save_contents['st_failures'] += 1
+            user_stats.update({"highest_count":number - 1})
+        user_stats.update({"failures":user_stats.get("failures", 0) + 1})
+        if user_stats.get("biggest_failure", 0) < number - 1:
+            user_stats.update({"biggest_failure":number - 1})
         number = 1
         last_author = 0
+        save_contents.update({f"st_user_{message.author.id}":user_stats})
         await message.channel.send(f"<@{message.author.id}> FAILED. THEY ARE A FAILURE. THEY SUCK. ETC.")
+        await save_state()
         return
     elif message_valid == 2:
         return
@@ -47,7 +62,10 @@ async def on_message(message):
     last_author = message.author.id
     
     number += 1
+    save_contents['st_counts'] += 1
+    user_stats.update({"total_counts":user_stats.get("total_counts", 0) + 1})
     await message.add_reaction("✅")
+    save_contents.update({f"st_user_{message.author.id}":user_stats})
     await save_state()
 
 async def on_message_edit(before, after):
@@ -76,8 +94,15 @@ async def save_state():
 
     save_contents['number'] = number
     save_contents['last_author'] = last_author
+    
+    if save_contents['st_highest_count'] < number - 1:
+        save_contents['st_highest_count'] = number - 1
+        save_contents['st_ruinedby'] = ""
 
-    file_contents = json.dumps(file_contents)
+    file_contents = json.dumps(save_contents)
     file.write(file_contents)
-    await log.info(f"counting: saved state\n```\n{file_contents}\n```")
+    await log.info(f"counting: saved state")
     file.close()
+
+async def get_savefile():
+    return save_contents
