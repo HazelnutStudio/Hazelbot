11dbb287f2aa34acdc1247a9a3105691573033df
HazelnutStudio <hazelnutstudio.yt@gmail.com>
2024-09-24 21:21:56 +0800

    no longer case sensitive

--- Hazelbot/Hazelbot/Hazelbot.cpp
+++ Hazelbot/Hazelbot/Hazelbot.cpp
@@ -1,6 +1,11 @@
 #include <dpp/dpp.h>
 
-const std::string v = "5";
+const std::string v = "6";
+
+std::string to_lower(std::string str) {
+	std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c) {return std::tolower(c);});
+	return str;
+}
 
 void text_interactions(dpp::cluster& bot, const dpp::message_create_t& event) {
 	if (event.msg.author == bot.me) {
@@ -8,8 +13,10 @@ void text_interactions(dpp::cluster& bot, const dpp::message_create_t& event) {
 		return;
 	}
 
+
+
 	// response - hazelbot
-	if (event.msg.content.find("hazelbot") != std::string::npos) {
+	if (to_lower(event.msg.content).find("hazelbot") != std::string::npos) {
 
 		// possible responses
 		const std::vector<std::string> responses = { "did someone say my name?", "hey ;]", "hello!!", ":3", "SHUT THE ###### #### ##### ###", "fine day innit?", "i think i've fallen in love with you", "thy shall suffer my wrath for proclaiming my name!", "haiii :3", "heyyyy", "i hate you.", "YOU MAKE EVEN THE DEVIL CRY", "i don't wanna talk rn..", "i'm about to go s*gma mode", "hi.", "hello", "greetings", "waow hi", "life has lost all meaning.", "^w^", "sorry i've been feeling a little upset lately and i don't really feel like talking :(", "shut up before i make you.", "hey", "haii how are you doing! ^^", "hai!!!", "yay! hi!", "ok.", "idc. shut up", "https://cdn.discordapp.com/attachments/1277125825447202816/1284132299150987274/IMG_9972.jpg?ex=66e584e6&is=66e43366&hm=d33b7f0e73eb89aeab7500018c15624d1213f9f8bf1e4f36d4fe012531893e6f&", "ok but did you hear about the rizzler" };
@@ -18,6 +25,8 @@ void text_interactions(dpp::cluster& bot, const dpp::message_create_t& event) {
 
 		event.reply(responses[r]);
 	}
+
+	// response "
 };
 
 int main(int argc, char* argv[]) {
