14434ed9b0348e5e52405fabae4450e53e542d03
hazel <hazelnutstudio.yt@gmail.com>
2024-10-26 18:16:13 +0800

    moved two to new file

--- Hazelbot.cpp
+++ Hazelbot.cpp
@@ -1,52 +1,18 @@
 ﻿#include "BotResponses.h"
 #include "Common.h"
-#include <cmath>
 #include "TimezoneOffsetFix.h"
 
 #include "Counting.h"
 #include "Clip.h"
 #include "ChatInteractions.h"
+#include "Two.h"
 
 #include "cmds/CStats.h"
 #include "cmds/Quote.h"
 
-std::string channel2id;
-
-bool two_filter(dpp::cluster& bot, const dpp::message_create_t& event) {
-	std::string channel_id = std::to_string(event.msg.channel_id);
-	if (channel_id != channel2id) {
-		// message was sent outside of #2, so it should be ignored
-		return false;
-	}
-	
-	if (event.msg.author == bot.me) {
-		// message was sent by bot, so it should be ignored
-		return false;
-	}
-
-	if (event.msg.content.find("2") != std::string::npos) {
-		return false;
-	}
-
-	if (event.msg.content.find("two") != std::string::npos) {
-		return false;
-	}
-
-	return true;
-}
-
-bool delete_commands = false;
 int main(int argc, char *argv[]) {
-
-  if(argc > 1){
-    if(std::strcmp(argv[1], "-delete_commands") == 0){
-      delete_commands = true;
-      std::cout << "deleting commands" << std::endl;
-    }
-  }
 	ConfigParser::initialize_configuration();
 	std::string token = ConfigParser::get_string("token", "");
-	channel2id = ConfigParser::get_string("2_id", "0");
 
   InitializeTimezoneOffset();
   InitializeResponses();
@@ -55,16 +21,11 @@ int main(int argc, char *argv[]) {
 
 	bot.on_log(dpp::utility::cout_logger());
 
-	// Message recieved
-	bot.on_message_create([&bot](const dpp::message_create_t& event) {
-		if (two_filter(bot, event)) {
-			bot.message_delete(event.msg.id, event.msg.channel_id);
-		}
-	});
-
   // initialize modules
   ChatInteractions interactions = ChatInteractions();
   bot.on_message_create(std::bind(&ChatInteractions::OnMessageSent, &interactions, std::placeholders::_1));
+
+  bot.on_message_create(&TwoOnMessageSent);
 
   Counting counting = Counting();
   bot.on_message_create(std::bind(&Counting::OnMessageCreate, &counting, std::placeholders::_1));
@@ -84,5 +45,4 @@ int main(int argc, char *argv[]) {
   bot.on_message_reaction_add(std::bind(&Quote::OnMessageReactionAdd, &cmd_quote, std::placeholders::_1));
 
 	bot.start(dpp::st_wait);
-	return 0;
 }
---
+++ Two.h
@@ -0,0 +1,8 @@
+#ifndef TWO_H
+#define TWO_H
+
+#include "Common.h"
+
+void TwoOnMessageSent(const dpp::message_create_t& message);
+
+#endif
