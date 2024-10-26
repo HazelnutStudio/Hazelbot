4a92f4aa7580ed259c324257a1412d46fc645358
hazel <hazelnutstudio.yt@gmail.com>
2024-10-26 17:31:56 +0800

    moved chat interactions to new file

---
+++ ChatInteractions.h
@@ -0,0 +1,11 @@
+#ifndef CHAT_INTERACTIONS_H
+#define CHAT_INTERACTIONS_H
+
+#include "Common.h"
+
+class ChatInteractions{
+public:
+  void OnMessageSent(const dpp::message_create_t& event);
+};
+
+#endif
--- Hazelbot.cpp
+++ Hazelbot.cpp
@@ -5,85 +5,12 @@
 
 #include "Counting.h"
 #include "Clip.h"
+#include "ChatInteractions.h"
 
 #include "cmds/CStats.h"
 #include "cmds/Quote.h"
 
 std::string channel2id;
-std::string clips_channel_id;
-
-void text_interactions(dpp::cluster& bot, const dpp::message_create_t& event) {
-	if (event.msg.author == bot.me) {
-		// message was sent by bot, so it should be ignored
-		return;
-	}
-
-	if (StringUtils::ends_with(event.msg.content, "?")) {
-		// is a question
-		const std::vector<std::string> question_startings = { "are", "do", "did", "can", "should", "would you", "could i", "am i", "is", "will", "won't you", "would", "wouldn't", "have" };
-		for (size_t i = 0; i < question_startings.size(); i++)
-		{
-			if (StringUtils::starts_with(StringUtils::to_lower(event.msg.content), "hazelbot, " + question_startings[i])) {
-				event.reply(GetResponse("eightball"));
-        return;
-			}
-
-			if (StringUtils::starts_with(StringUtils::to_lower(event.msg.content), "hazelbot " + question_startings[i])) {
-				event.reply(GetResponse("eightball"));
-				return;
-			}
-		}
-	}
-
-	// response - hazelbot (strikethrough)
-	if (StringUtils::to_lower(event.msg.content).find("~~hazelbot~~") != std::string::npos) {
-		event.reply(GetResponse("strikethrough"));
-	}
-	// response - hazelbot
-	else if (StringUtils::to_lower(event.msg.content).find("hazelbot") != std::string::npos) {
-		if (ConfigParser::get_boolean("catgirl_mode", true)) {
-			event.reply("meow");
-			return;
-		}
-    
-    event.reply(GetResponse("hazelbot"));
-	}
-
-	// response "silksong"
-	if (StringUtils::to_lower(event.msg.content).find("silksong") != std::string::npos) {
-    event.reply(GetResponse("silksong"));
-	}
-
-	// response - step 3
-	if (StringUtils::to_lower(event.msg.content).find("step 3") != std::string::npos) {
-		event.reply(GetResponse("SQUISH"));
-	}
-
-	// response - ping hazelbot
-	if (StringUtils::to_lower(event.msg.content).find("<@1269130556386578524>") != std::string::npos) {
-		event.reply(GetResponse("ping"));
-	}
-
-	// response - :3
-	if (event.msg.content == ":3") {
-		event.reply(":3");
-	}
-
-	// response - let there be cabbits
-	if (StringUtils::to_lower(event.msg.content) == "let there be cabbits!") {
-		event.reply(GetResponse("let there be cabbits"));
-	}
-
-	// response - cabbit
-	else if (StringUtils::to_lower(event.msg.content).find("cabbit") != std::string::npos) {
-		event.reply(GetResponse("cabbit"));
-	}
-
-	// debug
-	if (StringUtils::to_lower(event.msg.content).find("secret dialogue") != std::string::npos && ConfigParser::does_key_exist("secret_dialogue")) {
-		event.reply(ConfigParser::get_string("secret_dialogue", ":3"));
-	}
-};
 
 bool two_filter(dpp::cluster& bot, const dpp::message_create_t& event) {
 	std::string channel_id = std::to_string(event.msg.channel_id);
@@ -130,20 +57,21 @@ int main(int argc, char *argv[]) {
 
 	// Message recieved
 	bot.on_message_create([&bot](const dpp::message_create_t& event) {
-		text_interactions(bot, event);
 		if (two_filter(bot, event)) {
 			bot.message_delete(event.msg.id, event.msg.channel_id);
 		}
 	});
 
   // initialize modules
+  ChatInteractions interactions = ChatInteractions();
+  bot.on_message_create(std::bind(&ChatInteractions::OnMessageSent, &interactions, std::placeholders::_1));
+
   Counting counting = Counting();
   bot.on_message_create(std::bind(&Counting::OnMessageCreate, &counting, std::placeholders::_1));
 
   Clip clip = Clip();
   bot.on_message_create(std::bind(&Clip::OnMessageSent, &clip, std::placeholders::_1));
   bot.on_message_reaction_add(std::bind(&Clip::OnMessageReactionAdd, &clip, std::placeholders::_1));
-
   
   // initialize commands
   CStats cmd_cstats = CStats();
