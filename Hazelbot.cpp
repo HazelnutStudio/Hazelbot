03a5b7f61d4d4e1603d277026eee42088af8059c
hazel <hazelnutstudio.yt@gmail.com>
2024-10-27 18:53:07 +0800

    Fixed segmentation fault

--- Hazelbot.cpp
+++ Hazelbot.cpp
@@ -9,8 +9,6 @@
 #include "Commands/Quote.h"
 #include "Commands/Wisdom.h"
 
-dpp::cluster* Bot;
-
 int main(int argc, char* argv[]){
   // Initialize core libraries
   ConfigParser::initialize_configuration();
@@ -26,12 +24,9 @@ int main(int argc, char* argv[]){
     exit(INVALID_TOKEN);
   }
 
-  {
-    dpp::cluster bot(token, dpp::i_default_intents | dpp::i_message_content);
-    Bot = &bot;
-  }
+  dpp::cluster bot(token, dpp::i_default_intents | dpp::i_message_content);
 
-  Bot->on_log(dpp::utility::cout_logger());
+  bot.on_log(dpp::utility::cout_logger());
 
   // Initialize modules
   ChatInteractions mod_chatInteractions = ChatInteractions();
@@ -39,33 +34,33 @@ int main(int argc, char* argv[]){
   Clip mod_clip = Clip();
 
   // Set up module callbacks
-  Bot->on_message_create(&TwoOnMessageSent);
+  bot.on_message_create(&TwoOnMessageSent);
 
-  Bot->on_message_create(std::bind(&ChatInteractions::OnMessageSent, &mod_chatInteractions, std::placeholders::_1));
+  bot.on_message_create(std::bind(&ChatInteractions::OnMessageSent, &mod_chatInteractions, std::placeholders::_1));
 
-  Bot->on_message_create(std::bind(&Counting::OnMessageCreate, &mod_counting, std::placeholders::_1));
+  bot.on_message_create(std::bind(&Counting::OnMessageCreate, &mod_counting, std::placeholders::_1));
 
-  Bot->on_message_create(std::bind(&Clip::OnMessageSent, &mod_clip, std::placeholders::_1));
-  Bot->on_message_reaction_add(std::bind(&Clip::OnMessageReactionAdd, &mod_clip, std::placeholders::_1));
+  bot.on_message_create(std::bind(&Clip::OnMessageSent, &mod_clip, std::placeholders::_1));
+  bot.on_message_reaction_add(std::bind(&Clip::OnMessageReactionAdd, &mod_clip, std::placeholders::_1));
 
   // Initialize commands
   CStats cmd_cstats = CStats();
   Quote cmd_quote = Quote();
   Wisdom cmd_wisdom = Wisdom();
 
-  Bot->on_ready(&InitializeCommandRegisterer);
+  bot.on_ready(&InitializeCommandRegisterer);
 
   // Set up command callbacks
-  Bot->on_ready(std::bind(&CStats::InitializeCommand, &cmd_cstats, std::placeholders::_1, mod_counting));
-  Bot->on_slashcommand(std::bind(&CStats::OnCommandRun, &cmd_cstats, std::placeholders::_1));
+  bot.on_ready(std::bind(&CStats::InitializeCommand, &cmd_cstats, std::placeholders::_1, mod_counting));
+  bot.on_slashcommand(std::bind(&CStats::OnCommandRun, &cmd_cstats, std::placeholders::_1));
 
-  Bot->on_ready(std::bind(&Quote::InitializeCommand, &cmd_quote, std::placeholders::_1));
-  Bot->on_message_context_menu(std::bind(&Quote::OnCommandRun, &cmd_quote, std::placeholders::_1));
-  Bot->on_message_reaction_add(std::bind(&Quote::OnMessageReactionAdd, &cmd_quote, std::placeholders::_1));
+  bot.on_ready(std::bind(&Quote::InitializeCommand, &cmd_quote, std::placeholders::_1));
+  bot.on_message_context_menu(std::bind(&Quote::OnCommandRun, &cmd_quote, std::placeholders::_1));
+  bot.on_message_reaction_add(std::bind(&Quote::OnMessageReactionAdd, &cmd_quote, std::placeholders::_1));
 
-  Bot->on_ready(std::bind(&Wisdom::InitializeCommand, &cmd_wisdom, std::placeholders::_1));
-  Bot->on_slashcommand(std::bind(&Wisdom::OnCommandRun, &cmd_wisdom, std::placeholders::_1));
+  bot.on_ready(std::bind(&Wisdom::InitializeCommand, &cmd_wisdom, std::placeholders::_1));
+  bot.on_slashcommand(std::bind(&Wisdom::OnCommandRun, &cmd_wisdom, std::placeholders::_1));
 
   // Start bot
-  Bot->start(dpp::st_wait);
+  bot.start(dpp::st_wait);
 }
