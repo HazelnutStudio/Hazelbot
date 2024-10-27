239b9f9bc77f19453fde965470a5f4d3dd51ecb1
hazel <hazelnutstudio.yt@gmail.com>
2024-10-27 18:27:25 +0800

    Cleaned up entry point

--- Common.h
+++ Common.h
@@ -1,6 +1,11 @@
 #include <dpp/dpp.h>
+
 #include "StringUtils.h"
 #include "ConfigParser.h"
-#include "TimezoneOffsetFix.h"
 #include "BotResponses.h"
+
 #include "RegisterCommand.h"
+
+#include "TimezoneOffsetFix.h"
+
+#include "ErrorCodes.h"
