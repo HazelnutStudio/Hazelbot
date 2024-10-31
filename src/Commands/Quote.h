4a00a05e505a8096335bfc4948beabd0d97c5e2b
hazel <hazelnutstudio.yt@gmail.com>
2024-10-31 22:03:56 +0800

    Removed callback function definitions from header files

--- src/Commands/Quote.h
+++ src/Commands/Quote.h
@@ -8,8 +8,6 @@
 class Quote{
 private:
   void addQuoteMessage(Quote_MessageInfo& message, dpp::cluster* bot, std::string key);
-  void getQuoteVoteMessageCallback(const dpp::confirmation_callback_t& callback, const dpp::message_context_menu_t& event);
-  void getReactedMessageCallback(const dpp::confirmation_callback_t& callback, const dpp::message_reaction_add_t& event);
   int test;
 
 public:
--- src/Modules/Clip.h
+++ src/Modules/Clip.h
@@ -17,7 +17,6 @@ class Clip{
 private:
   bool isClipsMessage(dpp::snowflake channelId, dpp::snowflake messageId);
   void addTopClip(Clip_MessageInfo& message, dpp::cluster* bot, std::string key);
-  void onMessageReactionAddGetMessageCallback(const dpp::confirmation_callback_t& callback, const dpp::message_reaction_add_t& event);
 public:
   std::unordered_map<std::string, Clip_MessageInfo> ActiveVotes;
   void OnMessageSent(const dpp::message_create_t& event);
